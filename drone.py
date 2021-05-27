103c2a878377e7e1f6ef6e4f6d1f5e2fa4b72a5c
Hugo Gendarme <hugogendarme@MacBook-Pro-de-Hugo.local>
2021-05-27 19:55:42 +0200

    parity

--- drone.py
+++ drone.py
@@ -12,12 +12,13 @@ def list_to_adj_matrix(L, n):
 #------------------------------------------------------------------------------
 # (source, destination, weight)
 graph = [(0, 1, 10), (0, 2, 10), (1, 3, 7), (1, 4, 4), (2, 3, 5), (2,
-5, 5), (5, 6, 9), (4, 6, 12), (3, 6, 7), (2, 1, 3), (4, 0, 4), (4, 3, 2)]
+5, 5), (5, 6, 9), (4, 6, 12), (3, 6, 7), (2, 1, 3), (4, 0, 4), (4, 3, 2), (1,
+3, 6)]
 n = 7
 M = list_to_adj_matrix(graph, n)
 
 def find_odd_vertices(M, n):
-    parity = np.full(n, 0)
+    parity = [0] * n
     for u in range(n):
         for v in range(n):
             if u >= v:
@@ -25,6 +26,8 @@ def find_odd_vertices(M, n):
             if M[u][v] > 0:
                 parity[u] += 1
                 parity[v] += 1
+
+    print(parity)
 
     result = []
     for i in range(n):
@@ -36,7 +39,7 @@ def find_odd_vertices(M, n):
 def min_distance(M, meet, dist):
     min, min_index = np.inf, 0
 
-    for i in range(len(dist)):
+    for i in range(n):
         if dist[i] < min and meet[i] is False:
             min = dist[i]
             min_index = i
@@ -44,16 +47,21 @@ def min_distance(M, meet, dist):
     return min_index
 
 def dijkstra(M, s, d):
-    dist = np.full(n, np.inf)
+    dist = [float('inf')] * n
     dist[s] = 0
-    meet = np.full(n, False)
+    meet = [False] * n
 
     for curr in range(n):
         u = min_distance(M, meet, dist)
         meet[u] = True
 
         for v in range(n):
-            if u != v and meet[v] is False and dist[v] > dist[u] + M[u][v]:
+            #print(v)
+            #print(meet)
+            #print(M[u][v] > 0)
+            #print(meet[v] is False)
+            #print(dist[v] > dist[u] + M[u][v])
+            if M[u][v] > 0 and meet[v] is False and dist[v] > dist[u] + M[u][v]:
                 dist[v] = dist[u] + M[u][v]
 
     print(dist)
@@ -72,6 +80,7 @@ def find_minimum_pairing(odd):
 
     return result
 
+print(M)
 odd = find_odd_vertices(M, n)
 print(odd)
 print(find_minimum_pairing(odd))
