af93a25a8fe95d471a90c7ca80781812392e3d69
Etienne Chanudet <etienne.chanudet@epita.fr>
2021-05-30 23:03:54 +0200

    need find_minimum_pairing

--- theorical_case/snowplow.py
+++ theorical_case/snowplow.py
@@ -1,25 +1,25 @@
 import numpy as np
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import dijkstra
 
-def list_to_adj_matrix(L, n):
-    M = np.full((n, n), 0)
-    for s, d, w in L:
-        M[s][d] = w
-    return M
+def edges_to_matrix(edges, n):
+    matrix = np.zeros((n, n))
+    for s, d, w in edges:
+        matrix[s][d] = w
+    return matrix
 
-def find_odd_vertices(M, n):
-    parity = [0] * n
-    for s in range(n):
-        for d in range(n):
-            if M[s][d] > 0:
-                parity[s] += 1
-                parity[d] -= 1
+def find_unbalanced_vertices(edges, n):
+    parity = np.zeros(n)
+    for s, d, _ in edges:
+        parity[s] += 1
+        parity[d] -= 1
 
     in_co, out_co = [], []
     for i in range(n):
         if parity[i] > 0:
-            out_co.append(i)
+            out_co.append((i, int(abs(parity[i]))))
         elif parity[i] < 0:
-            in_co.append(i)
+            in_co.append((i, int(abs(parity[i]))))
     return (in_co, out_co)
 
 def find_min_distance(M, n, meet, dist):
@@ -48,4 +48,59 @@ def find_shortest_path(M, s):
                 dist[v] = dist[u] + M[u][v]
     return dist
 
-def find_minimum_pairing(M, odd):
+def find_minimum_pairing(graph, n, in_u, out_u):
+    #FIXME
+    pass
+
+def make_graph_eulerian(edges, n):
+    in_u, out_u = find_unbalanced_vertices(edges, n)
+    if not in_u and not out_u:
+        return
+
+    M = edges_to_matrix(edges, n)
+    pairs = find_minimum_pairing(M, n, in_u, out_u)
+    for s, d, w, path in pairs:
+        edges.append((s, d, int(w)))
+    return pairs
+
+def find_corresponding_pair(u, v, w, pairs):
+    for i in range(len(pairs)):
+        if u == pairs[i][0] and v == pairs[i][1] and w == pairs[i][2]:
+            return pairs.pop(i)
+    return None
+
+def find_eulerian_cycle(edges, pairs, n):
+    cycle = [edges[0][0]]
+    while True:
+        rest = []
+        for u, v, w in edges:
+            pair = find_corresponding_pair(u, v, w, pairs)
+            if cycle[-1] == u:
+                cycle.append(v)
+            else:
+                rest.append((u,v,w))
+        if not rest:
+            return cycle
+        edges = rest
+        if cycle[0] == cycle[-1]:
+            for u, v, _ in edges:
+                if u in cycle:
+                    idx = cycle.index(u)
+                    cycle = cycle[idx:-1] + cycle[0:idx+1]
+                    break
+
+##############################################################################
+
+edges = [(0, 1, 10), (0, 2, 10), (1, 3, 7), (1, 4, 4), (2, 3, 5), (2, 5, 5),\
+(5, 6, 7), (6, 4, 12), (3, 6, 9), (2, 1, 3), (4, 0, 4), (4, 3, 2), (1,5, 6),\
+(3, 2, 35), (3, 2, 35), (5, 0, 23), (6, 1, 26)]
+n = 7
+
+in_odd, out_odd = find_odd_vertices(edges, n)
+print((in_odd, out_odd))
+#M = edges_to_matrix(edges, n)
+#pairs = find_minimum_pairing(M, n, odd)
+#print(pairs)
+
+res = find_eulerian_cycle(edges, [], n)
+print(res)
