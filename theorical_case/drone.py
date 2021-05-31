d43d8835201d30686c2874c8418047e23640b69c
hugo.gendarme <hugo.gendarme@epita.fr>
2021-05-31 19:28:19 +0200

    fix path for drone, need to fix it for directed graph, must change floyd -> dijsktra

--- theorical_case/drone.py
+++ theorical_case/drone.py
@@ -2,13 +2,6 @@ import numpy as np
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import dijkstra
 
-def edges_to_matrix(edges, n):
-    matrix = np.zeros((n, n))
-    for s, d, w in edges:
-        matrix[s][d] = w
-        matrix[d][s] = w
-    return matrix
-
 def find_odd_vertices(edges, n):
     parity = np.zeros(n)
     for s, d, _ in edges:
@@ -20,6 +13,13 @@ def find_odd_vertices(edges, n):
         if parity[i] % 2 != 0:
             result.append(i)
     return result
+
+def edges_to_matrix(edges, n):
+    matrix = np.zeros((n, n))
+    for s, d, w in edges:
+        matrix[s][d] = w
+        matrix[d][s] = w
+    return matrix
 
 def get_path(prev, x, path):
     if prev[x] == -9999:
@@ -37,7 +37,8 @@ def find_minimum_pairing(graph, n, odd):
         min_path = []
         for v in range(len(odd)):
             if (u != v):
-                dist, prev = dijkstra(csgraph=graph, directed=False, indices=odd[u], return_predecessors=True)
+                dist, prev = dijkstra(csgraph=graph, directed=False, \
+                        indices=odd[u], return_predecessors=True)
                 paths = []
                 for d in range(n):
                     path = []
@@ -78,7 +79,8 @@ def make_graph_eulerian(edges, n):
 
 def find_corresponding_pair(u, v, w, pairs):
     for i in range(len(pairs)):
-        if (u == pairs[i][0] and v == pairs[i][1]) or (u == pairs[i][1] and v == pairs[i][0]) and w == pairs[i][2]:
+        if (u == pairs[i][0] and v == pairs[i][1]) \
+                or (u == pairs[i][1] and v == pairs[i][0]) and w == pairs[i][2]:
             return pairs.pop(i)
     return None
 
@@ -88,26 +90,27 @@ def find_eulerian_cycle(edges, pairs, n):
     while True:
         rest = []
         for u, v, w in edges:
-            pair = find_corresponding_pair(u, v, w, pairs)
-            # FIXME : replace artificial edges by the real path
+            path = find_corresponding_pair(u, v, w, pairs)
+            if path:
+                path = path[3]
             if cycle[-1] == u:
-                cycle.append(v)
-                if pair:
-                    for i in range(1, len(pair[3])):
-                        res.append((pair[3][i-1], pair[3][i]))
+                if path and len(path) > 2:
+                    cycle.pop()
+                    for x in path:
+                        cycle.append(x)
                 else:
-                    res.append((u,v))
+                    cycle.append(v)
             elif cycle[-1] == v:
-                cycle.append(u)
-                if pair:
-                    for i in range(1, len(pair[3])):
-                        res.append((pair[3][i-1], pair[3][i]))
+                if path and len(path) > 2:
+                    cycle.pop()
+                    for x in path:
+                        cycle.append(x)
                 else:
-                    res.append((v,u))
+                    cycle.append(u)
             else:
                 rest.append((u,v,w))
         if not rest:
-            return res
+            return cycle
         edges = rest
         if cycle[0] == cycle[-1]:
             for u, v, _ in edges:
@@ -115,3 +118,10 @@ def find_eulerian_cycle(edges, pairs, n):
                     idx = cycle.index(u)
                     cycle = cycle[idx:-1] + cycle[0:idx+1]
                     break
+
+edges = [(0, 1, 10), (0, 2, 10), (1, 3, 7), (1, 4, 4), (2, 3, 5), (2, 5, 5),\
+(5, 6, 7), (6, 4, 12), (3, 6, 9), (2, 1, 3), (4, 0, 4), (4, 3, 2), (1, 5, 6)]
+n = 7
+pairs = make_graph_eulerian(edges, n)
+res = find_eulerian_cycle(edges, pairs, n)
+print(res)
