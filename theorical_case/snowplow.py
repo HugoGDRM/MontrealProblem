ee2d0d86c26d58229345b9f3abb5ea836c5bad91
hugo.gendarme <hugo.gendarme@epita.fr>
2021-06-02 13:17:27 +0200

    fix directed matching

--- theorical_case/snowplow.py
+++ theorical_case/snowplow.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import dijkstra
 from scipy.sparse.csgraph import floyd_warshall
+from scipy.optimize import linear_sum_assignment
 
 def edges_to_matrix(edges, n):
     matrix = np.zeros((n, n))
@@ -31,23 +32,60 @@ def get_path(prev, x, path):
     path.append(x)
 
 def find_minimum_pairing(graph, n, out_u, in_u):
-    res = []
-    meet = set()
-    for s, sp in out_u:
-        dist, prev = dijkstra(csgraph=graph, directed=True, indices=s,\
-            return_predecessors=True)
+    OUT = []
+    for o, oN in out_u:
+        for x in range(oN):
+            OUT.append(o)
+    IN = []
+    for i, iN in in_u:
+        for x in range(iN):
+            IN.append(i)
 
-        for d, dp in in_u:
-            path = []
-            get_path(prev, d, path)
+    print("OUT", OUT)
+    print("IN", IN)
 
-            if sp == dp and str(s) not in meet and str(d) not in meet:
-                for i in range(sp):
-                    res.append((s, d, int(dist[d]), path))
-                    new_meeting = {str(s):'', str(d):''}
-                    meet.update(new_meeting)
+    dist_matrix = np.full((len(OUT), len(IN)), np.inf)
+    prevs = [[] for _ in range(n)]
 
-    return res
+    for s in range(len(OUT)):
+        dist, prev = dijkstra(csgraph=graph, directed=True,\
+                indices=OUT[s], return_predecessors=True)
+        prevs[OUT[s]] = prev
+        for d in range(len(IN)):
+            dist_matrix[s][d] = dist[IN[d]]
+
+    print(dist_matrix)
+    row_ind, col_ind = linear_sum_assignment(dist_matrix)
+    print(row_ind)
+    print(col_ind)
+
+    result = []
+    for i in range(len(row_ind)):
+        u, v = OUT[row_ind[i]], IN[col_ind[i]]
+        path = []
+        get_path(prevs[u], v, path)
+        result.append((u, v, dist[u][v], path))
+
+    print(result)
+    return result
+
+    #res = []
+    #meet = set()
+    #for s, sp in out_u:
+    #    dist, prev = dijkstra(csgraph=graph, directed=True, indices=s,\
+    #        return_predecessors=True)
+
+    #    for d, dp in in_u:
+    #        path = []
+    #        get_path(prev, d, path)
+
+    #        if sp == dp and str(s) not in meet and str(d) not in meet:
+    #            for i in range(sp):
+    #                res.append((s, d, int(dist[d]), path))
+    #                new_meeting = {str(s):'', str(d):''}
+    #                meet.update(new_meeting)
+
+    #return res
 
 def make_graph_eulerian(edges, n):
     out_u, in_u = find_unbalanced_vertices(edges, n)
@@ -99,12 +137,12 @@ def find_eulerian_cycle(edges, pairs, n):
 #
 #edges = [(0, 1, 10), (0, 2, 10), (1, 3, 7), (1, 4, 4), (2, 3, 5), (2, 5, 5),\
 #(5, 6, 7), (6, 4, 12), (3, 6, 9), (2, 1, 3), (4, 0, 4), (4, 3, 2), (1, 5, 6)]
-#n = 7
+edges = [(0,1,10),(0, 5, 4), (0,2,2), (2, 6, 8), (0,3,8),(0,4,1),(1,2,4)\
+        ,(3,1,10),(4,1,3),(3,2,6),(2,4,5),(4,3,2), (6, 0, 4), (5, 4, 8)]
+n = 7
 #
-#out_u, in_u = find_unbalanced_vertices(edges, n)
-#M = edges_to_matrix(edges, n)
-#print("OUT", out_u)
-#print("IN", in_u)
-#pairs = make_graph_eulerian(edges, n)
+out_u, in_u = find_unbalanced_vertices(edges, n)
+M = edges_to_matrix(edges, n)
+pairs = make_graph_eulerian(edges, n)
 #res = find_eulerian_cycle(edges, pairs, n)
 #print(res)
